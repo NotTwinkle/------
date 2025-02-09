c76c6b20af16274eacb5d4f8a0454659c2965353
Jeremiah Pator Panganoran <166945134+NotTwinkle@users.noreply.github.com>
2025-02-09 22:39:52 +0800

    Update Envelope.tsx

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -187,7 +187,7 @@ const Envelope: React.FC<EnvelopeProps> = () => {
                       className="letter-content relative"
                       initial={{ opacity: 0, y: 50 }}
                       animate={{ opacity: 1, y: 0 }}
-                      transition={{ duration: 0.8 }}
+                      transition={{ duration: 1.8 }}
                       style={{ marginTop: "30px" }}
                     >
                       <p
